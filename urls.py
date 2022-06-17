3a93f2da93343f6109fbd10da811dc43c9d4e948
navyasri-star <75940932+navyasri-star@users.noreply.github.com>
2022-06-17 12:57:48 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,28 +1,24 @@
-from django.urls import path
+"""pizza URL Configuration
 
-from . import views
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import include, path
 
-app_name = "orders"
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("login", views.login_request, name="login"),
-    path("register/", views.register, name="register"),
-    path("logout", views.logout_request, name="logout"),
-    path("Pizza", views.pizza, name="pizza"),
-    path("Pasta", views.pasta, name="pasta"),
-    path("Salad", views.salad, name="salad"),
-    path("Subs", views.subs, name="subs"),
-    path("Platters", views.dinner_platters, name="dinner_platters"),
-    path("directions", views.directions, name="directions"),
-    path("hours", views.hours, name="hours"),
-    path("contact", views.contact, name="contact"),
-    path("cart", views.cart, name="cart"),
-    path("checkout", views.checkout, name="checkout"),
-    path("view-orders", views.view_orders, name="view_orders"),
-    path("mark_order_as_delivered", views.mark_order_as_delivered, name="mark_order_as_delivered"),
-    path("save_cart", views.save_cart, name="save_cart"),
-    path("retrieve_saved_cart", views.retrieve_saved_cart, name="retrieve_saved_cart"),
-    path("check_superuser", views.check_superuser, name="check_superuser"),
-
+    path("", include("orders.urls")),
+    path("admin/", admin.site.urls),
+    path('tinymce/', include('tinymce.urls')),
 ]
